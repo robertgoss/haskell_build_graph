4f0c995b62aa0ac1d6a1229538f993d9241494fa
Robert <goss.robert@gmail.com>
2015-10-12 05:16:27 -0700

    Merging in package data branch

---
+++ src/Cabal/Package.hs
@@ -0,0 +1,98 @@
+module Cabal.Package where
+
+import Data.Version(Version)
+import Distribution.Package(PackageName, Dependency)
+import Distribution.Version(VersionRange)
+import Distribution.PackageDescription(Flag)
+
+import Data.Map(Map)
+
+import Cabal.Conditional
+
+--A series of data structures to represent a cabal package for building
+--We want to seperate the (pure) conditional package (etc) from the one which
+--is platform dependent. We use our own class here to clear up these destinctions
+-- Unlike in the cabal data structures. 
+--As we would like to have a database with information common to all
+--Also it helps us to have control over the data structures when using persistence.
+
+
+--A package is given by it's global data and the various build targets which are kept at the conditional level
+--As there are differing types of conditioning levels depending on if the result is global, on a package or resolved with all flags
+data PackageConditional conditional = Package {
+  globalProperties :: GlobalPackageData, --The global properties of the package.
+  --BuildTargets
+  library :: Maybe (Library conditional), -- Optional Conditional library build target.
+  executables :: [Executable conditional], -- Executable build targets kept in conditional form.
+  tests :: [TestSuite conditional], -- Executable build targets kept in conditional form.
+  benchmarks :: [Benchmark conditional], -- Executable build targets kept in conditional form.
+  --Flags the various flags availible and their deafault level
+  flags :: [Flag]
+}
+
+--Helper type
+-- The package has had no conditionals resolved is it not specifed to a platform
+type GlobalPackage = PackageConditional PlatformConditional
+
+
+--Helper types
+-- Some helper structured types for fields
+-- These add readability to descriptors
+-- They may be exteneded to newtypes to add validation
+-- For now we just use synonyms
+type Filename = String -- The system address of a file
+type WildcardFilename = String --The a pattern for the system address of some files
+                               -- must be in the form of dir/*.extension with no other wildcard.
+type URL = String --A web url address.
+type LicenseType = String -- The type of license used.
+
+--Represent the global package data from the
+--Not specific to any particular platform or build.
+data GlobalPackageData = GlobalPackageData {
+    --Global identifiers of this package.
+    name :: PackageName, -- The name of the package without version number
+    version :: Version, --The version number of the package
+    --Global package properties optional
+      --Package description
+    synopsis :: Maybe String, --Optional, short description of the package.
+    description :: Maybe String, --Optional, description of the package.
+    category :: Maybe String, --Optional, classification of the package with hackage category.
+    stability :: Maybe String, --Optional, the stability of the package alpha, beta etc.
+      --License information
+    license :: LicenseType, -- Type of the license this package uses.
+    copyright :: Maybe String, -- Optional, copyright notice.
+      --People and addresses.
+    author :: Maybe String, -- Optional, original author of the package.
+    maintainer :: Maybe String, -- Optional, email address of current package maintainer.
+    homepage :: Maybe URL, --Optional, homepage of the package.
+    bugReports :: Maybe URL, --Optional, webaddress to send bug reports to.
+    packageUrl :: Maybe URL --Optional, webaddress source of the package.
+}
+
+
+--A data structure to represent a cabal libray with fields wrapped in conditionals
+-- These conditionals may be trivial (ie boolean true).
+data Library conditional = Library {
+  libraryBuildDependencies :: [(conditional,Dependency)] --The build dependencies of this library dependent on the platform and flags
+}
+
+--A data structure to represent a cabal executable with fields wrapped in conditionals
+-- These conditionals may be trivial (ie boolean true).
+data Executable conditional = Executable {
+  executableTargetName :: String, --The name of this executable target
+  executableBuildDependencies :: [(conditional,Dependency)] --The build dependencies of this executable dependent on the platform and flags
+}
+
+--A data structure to represent a cabal test suite with fields wrapped in conditionals
+-- These conditionals may be trivial (ie boolean true).
+data TestSuite conditional = TestSuite {
+  testTargetName :: String, --The name of this test target
+  testBuildDependencies :: [(conditional,Dependency)] --The build dependencies of this test dependent on the platform and flags
+}
+
+--A data structure to represent a cabal benchmark with fields wrapped in conditionals
+-- These conditionals may be trivial (ie boolean true).
+data Benchmark conditional = Benchmark {
+  benchmarkTargetName :: String, --The name of this benchmark target
+  benchmarkBuildDependencies :: [(conditional,Dependency)] --The build dependencies of this benchmark dependent on the platform and flags
+}
---
+++ src/Cabal/Parsing.hs
@@ -0,0 +1,122 @@
+module Cabal.Parsing where
+
+import Distribution.Package
+import Distribution.PackageDescription.Parse
+import qualified Distribution.PackageDescription as PD
+
+import Cabal.Package
+import Cabal.Conditional
+
+--Use cabal parsing to get a generic package and then form a package global
+--Package parsing function
+--Throw away errors and warnings and turn into a maybe
+parsePackage :: String -> Maybe GlobalPackage
+parsePackage string = fmap fromGenericPackage parseMaybe
+  where parseResult = parsePackageDescription string
+        parseMaybe = case parseResult of
+                        (ParseFailed _) -> Nothing
+                        (ParseOk _ genericPackage) -> Just genericPackage
+
+--Convert a generic package to a global package
+fromGenericPackage :: PD.GenericPackageDescription -> GlobalPackage
+fromGenericPackage genericPackage = Package {
+                                        globalProperties = toGlobalPackageData (PD.packageDescription genericPackage),
+                                        flags = (PD.genPackageFlags genericPackage),
+                                        library = fmap toLibrary $ PD.condLibrary genericPackage,
+                                        executables = map toExecutable $ PD.condExecutables genericPackage,
+                                        tests = map toTest $ PD.condTestSuites genericPackage,
+                                        benchmarks = map toBenchmark $ PD.condBenchmarks genericPackage
+                                    }
+
+--Convert a package description to global properties
+--Map null entries to nothing.
+toGlobalPackageData :: PD.PackageDescription -> GlobalPackageData
+toGlobalPackageData packageDescription = GlobalPackageData {
+        name = pkgName (PD.package packageDescription),
+        version = pkgVersion (PD.package packageDescription),
+        synopsis = maybeNonNull (PD.synopsis packageDescription),
+        description = maybeNonNull (PD.description packageDescription),
+        category = maybeNonNull (PD.category packageDescription),
+        stability = maybeNonNull (PD.stability packageDescription),
+        license = show (PD.license packageDescription),
+        copyright = maybeNonNull (PD.copyright packageDescription),
+        homepage = maybeNonNull (PD.homepage packageDescription),
+        author = maybeNonNull (PD.author packageDescription),
+        maintainer = maybeNonNull (PD.maintainer packageDescription),
+        bugReports = maybeNonNull (PD.bugReports packageDescription),
+        packageUrl = maybeNonNull (PD.pkgUrl packageDescription)
+}
+   where maybeNonNull string | string /= "" = Just string
+                             | otherwise = Nothing
+
+--Parse conditional tree
+--Take the internal parsing tree and some property (list) and 
+-- construct the list of conditional, property pairs
+parseConditionalDependency :: PD.CondTree PD.ConfVar [Dependency] a -> [(PlatformConditional, Dependency)]
+parseConditionalDependency condNode = fixedConditonalDependencies ++ subComponetsDependencies
+   where fixedDependencies = PD.condTreeConstraints condNode
+         fixedConditonalDependencies = [(Logic True, dependence) | dependence <- fixedDependencies]
+         subComponents = PD.condTreeComponents condNode
+         subComponetsDependencies = concat $ map liftUpTree subComponents
+         --Take the pair of a conditional and an if / if else cond tree and map condtionals to lists of dependencies
+         --If branch
+         liftUpTree (cond, subTreeIf, Nothing) = ifConditionalDependencies
+           where conditional = toConditional cond
+                 --Parse the if sub tree then the newDependencies are the same with the additional condition condition.
+                 ifDependencies = parseConditionalDependency subTreeIf
+                 ifConditionalDependencies = [(And conditional subConditional, dependence) | (subConditional, dependence) <- ifDependencies]
+         liftUpTree (cond, subTreeIf, Just subTreeElse) = ifConditionalDependencies ++ elseConditionalDependencies
+           where conditional = toConditional cond
+                 --Parse the if sub tree then the newDependencies are the same with the additional condition condition.
+                 ifDependencies = parseConditionalDependency subTreeIf
+                 ifConditionalDependencies = [(And conditional subConditional, dependence) | (subConditional, dependence) <- ifDependencies]
+                 --Parse the else sub tree then the newDependencies are the same with the additional condition that condition if false.
+                 elseDependencies = parseConditionalDependency subTreeIf
+                 elseConditionalDependencies = [(And (Not conditional) subConditional, dependence) | (subConditional, dependence) <- elseDependencies]
+
+--Convert the cabal conditon into the platform conditional tree
+toConditional :: PD.Condition PD.ConfVar -> PlatformConditional
+--Convert variables over
+toConditional (PD.Var var) = case var of
+                                PD.OS os -> Var . Left $ OperatingSystem os
+                                PD.Arch arch -> Var . Left $ Architecture arch
+                                PD.Impl compiler version -> Var . Left . Implimentaton $ CompilerVersion compiler version
+                                PD.Flag flagName -> Var . Right . Flag $ show flagName
+--Convert logical over
+toConditional (PD.Lit lit) = Logic lit
+--Convert combinators over
+toConditional (PD.CNot cond) = Not $ toConditional cond
+toConditional (PD.CAnd cond1 cond2) = And (toConditional cond1) (toConditional cond2)
+toConditional (PD.COr cond1 cond2) = Or (toConditional cond1) (toConditional cond2)
+
+--Convert a conditional library in cabal into a Library with Platform conditional
+--Convert to a list of condition dependency pairs
+toLibrary :: PD.CondTree PD.ConfVar [Dependency] PD.Library -> Library PlatformConditional
+toLibrary condTree = Library {
+  libraryBuildDependencies = parseConditionalDependency condTree
+}
+
+--Convert a conditional executable in cabal into a Executable with Platform conditional
+--Convert to a list of condition dependency pairs
+toExecutable :: (String,PD.CondTree PD.ConfVar [Dependency] PD.Executable) -> Executable PlatformConditional
+toExecutable (name,condTree) = Executable {
+  executableTargetName = name,
+  executableBuildDependencies = parseConditionalDependency condTree
+}
+
+--Convert a conditional test in cabal into a Test with Platform conditional
+--Convert to a list of condition dependency pairs
+toTest :: (String, PD.CondTree PD.ConfVar [Dependency] PD.TestSuite) -> TestSuite PlatformConditional
+toTest (name,condTree) = TestSuite {
+  testTargetName = name,
+  testBuildDependencies = parseConditionalDependency condTree
+}
+
+--Convert a conditional benchmark in cabal into a Benchmark with Platform conditional
+--Convert to a list of condition dependency pairs
+toBenchmark :: (String, PD.CondTree PD.ConfVar [Dependency] PD.Benchmark) -> Benchmark PlatformConditional
+toBenchmark (name,condTree) = Benchmark {
+  benchmarkTargetName = name,
+  benchmarkBuildDependencies = parseConditionalDependency condTree
+}
+
